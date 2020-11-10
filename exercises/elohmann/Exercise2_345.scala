d1f426ff319f7914555f383ba1e3a8b2b37ed5f1
Lohmann, Eduard(elohmann) <None>
2020-11-10 15:38:21 +0100

    Fix comments

--- exercises/elohmann/Exercise2_345.scala
+++ exercises/elohmann/Exercise2_345.scala
@@ -7,11 +7,12 @@ object Exercise2_345 {
   private def curry[A,B,C]  (f: (A, B) => C)  : A => (B => C) =
     a => b => f(a,b)
 
-  // Exercise 2.3
+  // Exercise 2.4
 
   private def uncurry[A,B,C](f: A => B => C): (A, B) => C =
     (a,b) => f(a)(b)
 
+  // Exercise 2.5
 
   private def compose[A,B,C](f: B => C, g: A => B): A => C =
     a => f(g(a))
