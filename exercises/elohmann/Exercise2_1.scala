98227df0239711101138486b03406965c851f99f
Lohmann, Eduard(elohmann) <None>
2020-11-10 11:53:13 +0100

    Add tail recursive version of Fibonacci

--- exercises/elohmann/Exercise2_1.scala
+++ exercises/elohmann/Exercise2_1.scala
@@ -1,6 +1,22 @@
 object Exercise2_1 {
 
-  private def fibonacci(n: Int): Int = n match { // Exercise 2.1
+  private def tfibonacci(n:Int): Int = {
+
+    @annotation.tailrec
+    def fib(n:Int, a:Int, b:Int) : Int  = {
+      n match {
+        case _ if n < 0 => 0
+        case 0 => a
+        case 1 => b
+        case _ => fib(n-1,b, a+b)
+      }
+
+    }
+
+    fib(n, 0, 1)
+  }
+
+  private def fibonacci(n: Int): Int = n match {
     case _ if n <= 0 => 0
     case 1 => 1
     case _ => fibonacci(n - 1) + fibonacci(n - 2)
@@ -16,7 +32,7 @@ object Exercise2_1 {
       4 -> 3,
       5 -> 5,
       6 -> 8
-    ).forall { case (k, v) => v == fibonacci(k) }
+    ).forall { case (k, v) => v == tfibonacci(k) }
 
 
   def main(args: Array[String]): Unit = if (testFibonacci()) {
