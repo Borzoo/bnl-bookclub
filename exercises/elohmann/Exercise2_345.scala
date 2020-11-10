d15941f266ae44f0af69c6db5bb526a3f015d46c
Lohmann, Eduard(elohmann) <None>
2020-11-10 15:21:46 +0100

    Add uncurry and test combined

--- exercises/elohmann/Exercise2_345.scala
+++ exercises/elohmann/Exercise2_345.scala
@@ -1,47 +1,29 @@
 object Exercise2_345 {
 
+  private def repeat(n: Int, s: String) : String = s * n
+
   // Exercise 2.3
 
   private def curry[A,B,C]  (f: (A, B) => C)  : A => (B => C) =
     a => b => f(a,b)
-
-  private def repeat(n: Int, s: String) : String = s * n
-
-  private def testCurry() : Boolean = {
-      // A: Int
-      // B: String
-      // C: Seq[String]
-      val repeat2 = curry( repeat )(2)
-      val repeat0 = curry( repeat )(0)
-      val repeat1 = curry( repeat )(1)
-      val repeat5 = curry( repeat )(5)
-      val repeatN1 = curry( repeat )(-7)
-
-      repeat2("A") == "AA" &&
-      repeat2("b") == "bb" &&
-      repeat0("A") == "" &&
-      repeat1("A") == "A" &&
-      repeat5("A") == "AAAAA" &&
-      repeatN1("x") == "" &&
-      true
-  }
 
   // Exercise 2.3
 
   def uncurry[A,B,C](f: A => B => C): (A, B) => C =
     (a,b) => f(a)(b)
 
-  private def testUnCurry() : Boolean = {
-    // A: Int
-    // B: String
-    // C: Seq[String]
-    true
-  }
+  // Tests
 
   private def testAll() : Boolean = {
-    testCurry() &&
-    testUnCurry() &&
-    true
+    val repeatCurried: Int => String => String = curry(repeat)
+    val repeatCopy: (Int, String) => String = uncurry(repeatCurried)
+
+    Seq(
+      (-1, "AB"),
+      (0, "AB"),
+      (2, "AB"),
+      (9, "AB"),
+    ).forall { case (n, s) => repeatCopy(n, s) == repeat(n, s) }
   }
 
   def main(args: Array[String]): Unit = if (testAll()) {
