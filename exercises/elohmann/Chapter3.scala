77aead9cc4f48560b6b80f6a6bc4465c421dd782
Lohmann, Eduard(elohmann) <None>
2020-11-26 20:37:06 +0100

    Exercise 10

--- exercises/elohmann/Chapter3.scala
+++ exercises/elohmann/Chapter3.scala
@@ -1,5 +1,7 @@
 import Chapter3.Lijst.foldRight
 
+import scala.annotation.tailrec
+
 object Chapter3 {
 
   sealed trait Lijst[+E] {
@@ -16,10 +18,10 @@ object Chapter3 {
 
     // Question: How to write equals
 
-     def show[E](): String = {
-       def doIt(l: Lijst[E]): String = l match {
+    def show[E](): String = {
+      def doIt(l: Lijst[E]): String = l match {
         case Nil => "Empty"
-        case Cons(h, t) => h + ", " +  doIt(t)
+        case Cons(h, t) => h + ", " + doIt(t)
       }
 
       val s = doIt(this.asInstanceOf[Lijst[E]])
@@ -32,11 +34,11 @@ object Chapter3 {
   object Lijst {
 
 
-    /*********************** Exercise 3.1: *******************************/
+    /** ********************* Exercise 3.1: *******************************/
 
     // 3rd case 1+2 = 3
 
-    /*********************** Exercise 3.2: *******************************/
+    /** ********************* Exercise 3.2: *******************************/
 
     def tail[E](xs: Lijst[E]): Lijst[E] = xs match {
       case Nil => Nil
@@ -48,14 +50,14 @@ object Chapter3 {
       case Cons(_, xs) => Some(xs)
     }
 
-    /*********************** Exercise 3.3: *******************************/
+    /** ********************* Exercise 3.3: *******************************/
 
     def setHead[E](xs: Lijst[E], n: E): Lijst[E] = xs match {
       case Nil => Nil // There is no element to replace, otherwise Cons(n, Nil)
       case Cons(_, t) => Cons(n, t)
     }
 
-    /*********************** Exercise 3.4: *******************************/
+    /** ********************* Exercise 3.4: *******************************/
 
 
     def drop[E](xs: Lijst[E], n: Int): Lijst[E] = if (n < 1) xs
@@ -64,7 +66,7 @@ object Chapter3 {
       case Cons(_, t) => drop(t, n - 1)
     }
 
-    /*********************** Exercise 3.5: *******************************/
+    /** ********************* Exercise 3.5: *******************************/
 
     def dropWhile[E](xs: Lijst[E], f: E => Boolean): Lijst[E] = xs match {
       case Nil => Nil
@@ -72,7 +74,7 @@ object Chapter3 {
       case Cons(h, t) if !f(h) => Cons(h, t)
     }
 
-    /*********************** Exercise 3.6: *******************************/
+    /** ********************* Exercise 3.6: *******************************/
 
     def init[E](l: Lijst[E]): Lijst[E] = l match {
       case Nil => Nil
@@ -82,43 +84,50 @@ object Chapter3 {
 
     // We have no access to the (second to) last element, except walking the list
 
-    /*********************** Exercise 3.7: *******************************/
+    /** ********************* Exercise 3.7: *******************************/
 
-    def foldRight[A,B](l: Lijst[A], z: B)(f: (A,B) => B): B = l match {
+    def foldRight[A, B](l: Lijst[A], z: B)(f: (A, B) => B): B = l match {
       case Nil => z
-      case Cons(h,t) => f(h, foldRight(t,z)(f))
+      case Cons(h, t) => f(h, foldRight(t, z)(f))
     }
 
     def product(l: Lijst[Double]): Double = foldRight(l, 1.0)(_ * _)
 
     // This foldRight always recurs to the end of the list so no shortcutting
 
-    def shortcutFoldRight[E,R](l: Lijst[E], z: R, s: E)(f: (E,R) => R) : R = {
+    def shortcutFoldRight[E, R](l: Lijst[E], z: R, s: E)(f: (E, R) => R): R = {
       print(".")
       l match {
         case Nil => z
-        case Cons(x,_) if x == s => f(x,z)  // Question: case Cons(s,_) => f(s,z) does not work
-        case Cons(h,t) => f(h,shortcutFoldRight(t,z,s)(f))
+        case Cons(x, _) if x == s => f(x, z) // Question: case Cons(s,_) => f(s,z) does not work
+        case Cons(h, t) => f(h, shortcutFoldRight(t, z, s)(f))
       }
     }
 
     def shortcutProduct(l: Lijst[Double]): Double = shortcutFoldRight(l, 1.0, 0.0)(_ * _)
 
-    /*********************** Exercise 3.8: *******************************/
+    /** ********************* Exercise 3.8: *******************************/
 
     // foldRight(Lijst(1,2,3), Nil:Lijst[Int])(Cons(_,_)) = Lijst(1,2,3)
     // They are right associative?
 
-    /*********************** Exercise 3.9: *******************************/
+    /** ********************* Exercise 3.9: *******************************/
 
-    def length[E](xs: Lijst[E]) : Int = foldRight(xs, 0)( (_,l) => l+1 )
+    def length[E](xs: Lijst[E]): Int = foldRight(xs, 0)((_, l) => l + 1)
+
+    /** ********************* Exercise 3.10: *******************************/
+
+    @tailrec
+    def foldLeft[E, R](l: Lijst[E], z: R)(f: (R, E) => R): R = l match {
+      case Nil => z
+      case Cons(h, t) => foldLeft(t, f(z, h))(f)
+    }
 
     // Utility
 
     def apply[E](xs: E*): Lijst[E] =
       if (xs.isEmpty) Nil
       else Cons(xs.head, apply(xs.tail: _*))
-
   }
 
   def main(args: Array[String]): Unit = {
@@ -126,13 +135,13 @@ object Chapter3 {
     val short = Lijst(1, 2, 3, 4, 5)
     val long = Lijst(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
-    def display[E](m:String, l: Lijst[E]) : Unit = println( s"$m: ${l.show[E]()}" )
+    def display[E](m: String, l: Lijst[E]): Unit = println(s"$m: ${l.show[E]()}")
 
-    display("tail(Nil)", Lijst.tail(Nil) )
-    display("tail(short)", Lijst.tail(short) )
+    display("tail(Nil)", Lijst.tail(Nil))
+    display("tail(short)", Lijst.tail(short))
 
-    println("maybeTail(Nil): " + Lijst.maybeTail(Nil).map { l => s"Some(${l.show})" }.getOrElse( "None") )
-    println("maybeTail(short): " + Lijst.maybeTail(short).map { l => s"Some(${l.show})" }.getOrElse( "None") )
+    println("maybeTail(Nil): " + Lijst.maybeTail(Nil).map { l => s"Some(${l.show})" }.getOrElse("None"))
+    println("maybeTail(short): " + Lijst.maybeTail(short).map { l => s"Some(${l.show})" }.getOrElse("None"))
 
     display("setHead(Nil,0)", Lijst.setHead(Nil, 0))
     display("setHead(short,0)", Lijst.setHead(short, 0))
@@ -160,14 +169,20 @@ object Chapter3 {
       1.0, 2.0, 0.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0
     )
 
-    println(s"product(ds0): ${Lijst.product(ds0)}" )
-    println(s"shortcutProduct(ds0): ${Lijst.shortcutProduct(ds0)}" )
+    println(s"product(ds0): ${Lijst.product(ds0)}")
+    println(s"shortcutProduct(ds0): ${Lijst.shortcutProduct(ds0)}")
 
-    println("exercise 3.8: ", foldRight(Lijst(1,2,3), Nil:Lijst[Int])(Cons(_,_)) )
+    println("exercise 3.8: ", foldRight(Lijst(1, 2, 3), Nil: Lijst[Int])(Cons(_, _)))
 
     println(s"length(short) = ${Lijst.length(short)}")
     println(s"length(long) = ${Lijst.length(long)}")
     println(s"length(ds0) = ${Lijst.length(ds0)}")
+
+    println(s"foldLeft(1,2,3,4,5)(+) = ${Lijst.foldLeft(short, 0)(_+ _)}")
+    println(s"foldLeft(1,2,3,4,5)(-) = ${Lijst.foldLeft(short, 0)(_ - _)}")
+
+    println(s"foldRight(1,2,3,4,5)(+) = ${Lijst.foldRight(short, 0)(_+ _)}")
+    println(s"foldRight(1,2,3,4,5)(-) = ${Lijst.foldRight(short, 0)(_ - _)}")
 
     println("Ok")
   }
